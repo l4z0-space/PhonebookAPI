90aa894128202d087f1366243b48d1160370685b
l4z0-space <lazaronshyta@gmail.com>
2020-10-22 15:07:02 +0200

    3.6 phonebookAPI

---
+++ index.js
@@ -0,0 +1,98 @@
+const { response, request } = require('express')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+
+
+persons= [
+    {
+    "name": "Arto Hellas",
+    "number": "031-313412-12",
+    "id": 1
+    },
+    {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+    },
+    {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+    },
+    {
+    "name": "Lazaron Shyta",
+    "number": "00355-696703885",
+    "id": 5
+    }
+]
+
+const generateID = () => {
+    return Math.floor(Math.random() * 15000)
+}
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+  
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if(!person){
+        response.status(404).end()
+    }
+    else {
+        console.log(person);
+        response.json(person)
+    }
+})
+
+app.get('/info', (request, response) => {
+    const numOfPeople = persons.length
+    const message = `Phonebook has info for ${numOfPeople} people`
+    const date = new Date()
+    
+    const toSend = {
+        message, date
+    }
+    response.json(toSend)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body.name)
+    if(!body.name || !body.number){
+        response.status(400).json({
+            error: "Name or number are missing"
+        })
+    }
+
+    // Check if name entered has a duplicate
+    if ( persons.includes( persons.find(person => person.name == body.name))){
+        response.status(400).json({
+            error: "Cannot contain duplicate names."
+            })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateID()
+    }
+    console.log(persons)
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running in port ${PORT}`);
+})
