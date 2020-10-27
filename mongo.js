f40b0fc907605dc7c590227a831438e0a1a23606
l4z0-space <lazaronshyta@gmail.com>
2020-10-27 14:15:04 +0100

    3.12 CLI DB

---
+++ mongo.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3 ){
+    console.log('Provide arguments [ password or contact ] ')
+    process.exit(1)
+}
+
+
+const password = process.argv[2]
+const url =
+  `mongodb+srv://fullstack:${password}@fullstack.rso5f.mongodb.net/contactDB?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+  })
+  
+const Contact = mongoose.model('Contact', contactSchema)
+
+
+if ( process.argv.length === 3 ){
+    
+    console.log('Phonebook: ')
+
+    Contact.find({}).then(cont => {
+        allNames = cont.map(con => con.name)
+        allNames.forEach(person => {
+            console.log(person);
+        })
+        mongoose.connection.close()
+    })
+}
+
+else{
+
+    name = process.argv[3]
+    number = process.argv[4]
+
+    contact = new Contact({
+        name,
+        number
+    })
+
+    contact.save().then(result => {
+        console.log(`${result.name} saved!`);
+        mongoose.connection.close()
+    })
+}
+
+
